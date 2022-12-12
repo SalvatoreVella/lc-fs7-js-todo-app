42f8f53c69b30002a1666894585afb3b189de628
Camilla Scarton <camy.s96@gmail.com>
2022-12-12 10:23:14 +0100

    memory utility added

--- index.mjs
+++ index.mjs
@@ -1,24 +1,35 @@
 import { Costants } from "./constants.mjs";
+import { internalCache } from "./internalCache.mjs";
 
-const state ={
+let state ={
     todos: [],
     _todos: [],
+}
+
+const getMemoryState = async () => {
+    const memoryTodos = internalCache.get("state");
+    if (memoryTodos) {
+        state = {...memoryTodos};
+    } else {
+        await fetchTodos();
+    }
 }
 
 const fetchTodos = async () => {
     try{
         const results = await fetch(Costants.API_TODOS_URL);
         const _todos = await results.json();
-        const todos = [..._todos].splice(0,10);
+        const todos = [..._todos].splice(0,5);
         state.todos = [...todos];
         state._todos = [...todos];
+        internalCache.set("state", state);
     }catch(error){
         console.log(error);
     }
 }
 
 const init = async() => {
-    await fetchTodos();
+    await getMemoryState();
     console.log(state);
 }
 
---
+++ internalCache.mjs
@@ -0,0 +1,11 @@
+export const internalCache = {
+    set: (name, data) => {
+        localStorage.setItem(name, JSON.stringify(data));
+    },
+    get: (name) => {
+        return JSON.parse(localStorage.getItem(name));
+    },
+    remove: (name) => {
+        localStorage.removeItem(name);
+    }
+};
